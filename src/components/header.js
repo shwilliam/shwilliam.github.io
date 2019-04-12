13ad04d343fa0821f0f14d6b326114080af26ba7
William L <w-lindvall@outlook.com>
2019-04-11 23:01:01 -0700

    clear category on title click

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,47 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import CategoryFilters from './category-filters'
 import SearchForm from './search-form'
+import FilterContext from '../context/filter-context'
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className="dib w-100 w-30-ns bb br-ns relative">
-      <section>
-        <h1 className="f4-ns f3 ma0">
-          <Link to="/" className="link near-black ttc db ph3 pb3 pt4 nowrap" tabIndex="-1">
+const Header = ({ siteTitle }) => {
+  const { setCategory } = useContext(FilterContext)
+
+  return (
+    <header>
+      <div className="dib w-100 w-30-ns bb br-ns relative">
+        <section>
+          <h1 className="f4-ns f3 ma0">
+            <Link to="/" onClick={() => setCategory()} className="link near-black ttc db ph3 pb3 pt4 nowrap" tabIndex="-1">
             William L<span className="dn-m">indvall</span>
-          </Link>
-        </h1>
-      </section>
-      <section>
-        <a className="dn-ns absolute right-1 top-1 link near-black pa3 f4-ns f3" href="mailto:w-lindvall@outlook.com">
-          <span role="img" aria-label="Email">
+            </Link>
+          </h1>
+        </section>
+        <section>
+          <a className="dn-ns absolute right-1 top-1 link near-black pa3 f4-ns f3" href="mailto:w-lindvall@outlook.com">
+            <span role="img" aria-label="Email">
             📧
-          </span>
+            </span>
+          </a>
+        </section>
+      </div>
+      <section className="dib-ns dn fr bb tr-ns w-100 w-70-ns">
+        <a className="link near-black ph3 pb3-ns pt4-ns pv2 dib" href="mailto:w-lindvall@outlook.com">
+        Contact me
         </a>
       </section>
-    </div>
-    <section className="dib-ns dn fr bb tr-ns w-100 w-70-ns">
-      <a className="link near-black ph3 pb3-ns pt4-ns pv2 dib" href="mailto:w-lindvall@outlook.com">
-        Contact me
-      </a>
-    </section>
-    <div>
-      <section className="dib w-100 f4 w-30-ns ma0 bb br-ns">
-        <CategoryFilters />
-      </section>
-      <section className="fr tr-ns w-100 w-70-ns bb">
-        <SearchForm />
-      </section>
-    </div>
-  </header>
-)
+      <div>
+        <section className="dib w-100 f4 w-30-ns ma0 bb br-ns">
+          <CategoryFilters />
+        </section>
+        <section className="fr tr-ns w-100 w-70-ns bb">
+          <SearchForm />
+        </section>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string.isRequired
