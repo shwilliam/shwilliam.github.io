bf1565c6bc4e773b67dbbd8499da3f3f72859811
William L <w-lindvall@outlook.com>
2019-04-11 22:55:32 -0700

    update welcome text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,31 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`home`]} />
-    <h2 className="dib-ns dn">
-      Select a project from the left to get started...
-    </h2>
+    <section className="dib-ns dn">
+      <span
+        className="f1 pa2 fr"
+        role="img"
+        aria-label="wave"
+      >
+      👋
+      </span>
+      <p className="f3 pv3 pv4-l mt4 mh3 mh4-l">
+        Hej!
+        <br /><br />
+          I&#8217;m William (usually{' '}
+        <a className="link" href="https://github.com/shwilliam" target="_blank" rel="noopener noreferrer">
+          @shwilliam
+        </a>
+        {' '}on the internet). I&nbsp;love the web and working to make it more fun, interesting
+        and exciting to use.
+        <br /><br />
+        Select a project from the left to read a bit more about it, or{' '}
+        <a className="link" href="mailto:w-lindvall@outlook.com">
+          get it touch
+        </a>
+        {' '}if you&#8217;re looking for something in particular!
+      </p>
+    </section>
   </Layout>
 )
 
