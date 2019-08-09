3242952bd59df2a6077a556be5262f5bc060c742
William L <w-lindvall@outlook.com>
2019-08-09 16:54:40 -0700

    avoid creating project pages for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,39 +20,40 @@ exports.onCreateWebpackConfig = ({
   })
 }
 
-exports.createPages = ({boundActionCreators, graphql}) => {
-  const {createPage} = boundActionCreators
+// creates project pages from template
+// exports.createPages = ({boundActionCreators, graphql}) => {
+//   const {createPage} = boundActionCreators
 
-  const projectTemplate = path.resolve('src/templates/project.js')
+//   const projectTemplate = path.resolve('src/templates/project.js')
 
-  return graphql(`
-    {
-      allMarkdownRemark(
-        sort: {order: DESC, fields: [frontmatter___date]}
-        limit: 1000
-        filter: {fileAbsolutePath: {regex: "/(/projects).*\\\\.md/"}}
-      ) {
-        edges {
-          node {
-            frontmatter {
-              date
-              path
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+//   return graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: {order: DESC, fields: [frontmatter___date]}
+//         limit: 1000
+//         filter: {fileAbsolutePath: {regex: "/(/projects).*\\\\.md/"}}
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               date
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       return Promise.reject(result.errors)
+//     }
 
-    result.data.allMarkdownRemark.edges.forEach(({node}) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: projectTemplate,
-        context: {},
-      })
-    })
-  })
-}
+//     result.data.allMarkdownRemark.edges.forEach(({node}) => {
+//       createPage({
+//         path: node.frontmatter.path,
+//         component: projectTemplate,
+//         context: {},
+//       })
+//     })
+//   })
+// }
