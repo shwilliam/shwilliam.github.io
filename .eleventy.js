93bed12162892f67ed8f5b0ab14a3cc18749830f
William L <w-lindvall@outlook.com>
2019-11-27 11:46:57 -0800

    generate rss

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,18 @@ module.exports = function(config) {
 
   config.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
 
+  config.addPlugin(require('@11ty/eleventy-plugin-rss'))
+
+  config.addLiquidFilter(
+    'latest',
+    d =>
+      d.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())[
+        d.length - 1
+      ],
+  )
+
+  config.addLiquidFilter('toIsoString', d => new Date(d).toISOString())
+
   return {
     dir: {
       input: 'src',
