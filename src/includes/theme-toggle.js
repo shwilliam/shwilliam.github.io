470d7fd175b3c1f6bd836530df164425388d0cda
William L <w-lindvall@outlook.com>
2020-01-13 15:21:14 -0800

    set up theme toggle

---
+++ src/includes/theme-toggle.js
@@ -0,0 +1,18 @@
+const htmlEl = document.firstElementChild
+const themeToggleEl = document.getElementById('theme-toggle')
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+htmlEl.dataset.theme = localStorage.getItem('theme')
+
+themeToggleEl.addEventListener('click', () => {
+  const theme = htmlEl.dataset.theme
+    ? htmlEl.dataset.theme === 'light'
+      ? 'dark'
+      : 'light'
+    : prefersDark
+    ? 'light'
+    : 'dark'
+
+  htmlEl.dataset.theme = theme
+  localStorage.setItem('theme', theme)
+})
