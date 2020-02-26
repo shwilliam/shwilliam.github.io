bd99230ca110ed939b0cfad62894fc42ed73c9fc
William L <w-lindvall@outlook.com>
2020-02-26 13:36:29 -0800

    fout with data attr

---
+++ src/includes/js/fonts.js
@@ -0,0 +1,12 @@
+if (sessionStorage && sessionStorage.fontsLoaded) {
+  document.documentElement.dataset.fonts = 'done'
+} else if ('fonts' in document) {
+  Promise.all([
+    document.fonts.load('1em MyAvenir'),
+    document.fonts.load('500 1em MyAvenir'),
+    document.fonts.load('italic 1em MyAvenir'),
+  ]).then(() => {
+    document.documentElement.dataset.fonts = 'done'
+    sessionStorage.fontsLoaded = true
+  })
+}
