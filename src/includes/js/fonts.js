e0cca69adb74fe9fc0d0a55bee6893f16d487f94
William L <w-lindvall@outlook.com>
2020-02-27 02:07:25 -0800

    wrap font loading in iife

--- src/includes/js/fonts.js
+++ src/includes/js/fonts.js
@@ -1,12 +1,14 @@
-if (sessionStorage && sessionStorage.fontsLoaded) {
-  document.documentElement.dataset.fonts = 'done'
-} else if ('fonts' in document) {
-  Promise.all([
-    document.fonts.load('1em MyAvenir'),
-    document.fonts.load('500 1em MyAvenir'),
-    document.fonts.load('italic 1em MyAvenir'),
-  ]).then(() => {
+;(() => {
+  if (sessionStorage && sessionStorage.fontsLoaded) {
     document.documentElement.dataset.fonts = 'done'
-    sessionStorage.fontsLoaded = true
-  })
-}
+  } else if ('fonts' in document) {
+    Promise.all([
+      document.fonts.load('1em MyAvenir'),
+      document.fonts.load('500 1em MyAvenir'),
+      document.fonts.load('italic 1em MyAvenir'),
+    ]).then(() => {
+      document.documentElement.dataset.fonts = 'done'
+      sessionStorage.fontsLoaded = true
+    })
+  }
+})()
