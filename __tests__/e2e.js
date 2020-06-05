6484d99c65065e4712424b318251f3e4526e0ed9
shwilliam <shwilliam@users.noreply.github.com>
2020-06-05 09:18:24 +0000

    Automated deployment: Fri Jun  5 09:18:24 UTC 2020 97fa1ced3f5ee919a92750fd55db131da6874c85

---
+++ __tests__/e2e.js
@@ -0,0 +1,119 @@
+const puppeteer = require('puppeteer')
+const LIVE_URL = 'https://shwilliam.com'
+const BROWSER_DIMENSIONS = {
+  width: 360,
+  height: 640,
+}
+
+let browser
+let page
+let logs = []
+let errors = []
+
+beforeAll(async () => {
+  browser = await puppeteer.launch(isDebugging())
+  page = await browser.newPage()
+
+  page.on('console', ({text}) => {
+    console.log({text})
+    logs.push(text)
+  })
+  page.on('pageerror', e => errors.push(e.text))
+
+  await page.goto(LIVE_URL)
+  page.setViewport(BROWSER_DIMENSIONS)
+})
+
+beforeEach(async () => {
+  errors = []
+  logs = []
+})
+
+describe('home page', () => {
+  test(`renders h1 with text 'Home'`, async () => {
+    const html = await page.$eval('h1', e => e.innerHTML)
+    expect(html).toContain('Home')
+  }, 16000)
+  test('no console logs', async () => {
+    expect(logs.length).toBe(0)
+  })
+  test('no exceptions', async () => {
+    expect(errors.length).toBe(0)
+  })
+})
+
+describe('404 page', () => {
+  beforeAll(async () => {
+    await page.goto(`${LIVE_URL}/sdflkvsm`)
+  })
+  test(`renders h1 with text '404'`, async () => {
+    const html = await page.$eval('h1', e => e.innerHTML)
+    expect(html).toContain('404')
+  }, 16000)
+  test('no exceptions', async () => {
+    expect(errors.length).toBe(0)
+  })
+})
+
+describe('libraries page', () => {
+  beforeAll(async () => {
+    await page.goto(`${LIVE_URL}/libraries`)
+  })
+  test(`renders h1 with text 'Libraries & Components'`, async () => {
+    const html = await page.$eval('h1', e => e.innerHTML)
+    expect(html).toContain('Libraries &amp; Components')
+  }, 16000)
+  test('no console logs', async () => {
+    expect(logs.length).toBe(0)
+  })
+  test('no exceptions', async () => {
+    expect(errors.length).toBe(0)
+  })
+})
+
+describe('projects page', () => {
+  beforeAll(async () => {
+    await page.goto(`${LIVE_URL}/projects`)
+  })
+  test(`renders h1 with text 'Websites & Apps'`, async () => {
+    const html = await page.$eval('h1', e => e.innerHTML)
+    expect(html).toContain('Websites &amp; Apps')
+  }, 16000)
+  test('no console logs', async () => {
+    expect(logs.length).toBe(0)
+  })
+  test('no exceptions', async () => {
+    expect(errors.length).toBe(0)
+  })
+})
+
+describe('blog page', () => {
+  beforeAll(async () => {
+    await page.goto(`${LIVE_URL}/blog`)
+  })
+  test(`renders h1 with text 'Blog'`, async () => {
+    const html = await page.$eval('h1', e => e.innerHTML)
+    expect(html).toContain('Recent Posts')
+  }, 16000)
+  test('no console logs', async () => {
+    expect(logs.length).toBe(0)
+  })
+  test('no exceptions', async () => {
+    expect(errors.length).toBe(0)
+  })
+})
+
+afterAll(() => {
+  if (isDebugging()) {
+    browser.close()
+  }
+})
+
+const isDebugging = () =>
+  process.env.NODE_ENV === 'debug'
+    ? {
+        headless: false,
+        slowMo: 250,
+        devtools: true,
+      }
+    : {}
