b11f307663bd0a7bb588290b5c931ce85946a09a
William L <w-lindvall@outlook.com>
2020-06-13 17:11:02 -0700

    scrape yearly npm downloads

---
+++ src/data/npm.js
@@ -0,0 +1,24 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare()
+
+module.exports = async function() {
+  const URL = 'https://npm-stat.com/charts.html?author=shwilliam'
+  const selector = 'table.alternating tr:last-of-type td:last-of-type'
+  let downloads
+
+  try {
+    downloads = await nightmare
+      .goto(URL)
+      .wait('table.alternating')
+      .wait(3000)
+      .evaluate(
+        selector => document.querySelector(selector).innerText,
+        selector,
+      )
+      .end()
+  } catch (error) {
+    console.error(error)
+  }
+
+  return downloads
+}
