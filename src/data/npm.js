0b44324c15d48faa10038fa10508b39df42b4a79
William Lindvall <shwilliam@hey.com>
2020-07-04 13:43:00 -0700

    preformat npm downloads fallback

--- src/data/npm.js
+++ src/data/npm.js
@@ -4,7 +4,7 @@ const nightmare = Nightmare()
 module.exports = async function() {
   const URL = 'https://npm-stat.com/charts.html?author=shwilliam'
   const selector = 'table.alternating tr:last-of-type td:last-of-type'
-  let downloads = 23191 // 202-07-02
+  let downloads = '23,191' // 202-07-02
 
   try {
     downloads = await nightmare
